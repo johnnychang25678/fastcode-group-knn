5fde32e9451e6db724c1aafc94b56d25da15b4d2
kuouu <kuouu.tw@gmail.com>
2024-05-01 09:36:10 -0400

    refactor feature test

--- data/feature_test.py
+++ data/feature_test.py
@@ -4,10 +4,10 @@ def main():
     generate_test_suite([
         ("feature_train1", 16384, 3),
         ("feature_test1", 4096, 3),
-        ("feature_train2", 16384, 10),
-        ("feature_test2", 4096, 10),
-        ("feature_train3", 16384, 100),
-        ("feature_test3", 4096, 100)
+        ("feature_train2", 16384, 100),
+        ("feature_test2", 4096, 100),
+        ("feature_train3", 16384, 500),
+        ("feature_test3", 4096, 500)
     ])
 
 if __name__ == "__main__":
--- test/feature.sh
+++ test/feature.sh
@@ -5,22 +5,6 @@ cd data
 python3 feature_test.py
 cd ..
 
-# test baseline
-cd baseline
-make clean && make
-baseline_result1=$(./bin/main run ../data/feature_train1 ../data/feature_test1 3)
-baseline_result2=$(./bin/main run ../data/feature_train2 ../data/feature_test2 3)
-baseline_result3=$(./bin/main run ../data/feature_train3 ../data/feature_test3 3)
-cd ..
-
-# test optimized
-cd optimized
-make clean && make
-optimized_result1=$(./bin/main run ../data/feature_train1 ../data/feature_test1 3)
-optimized_result2=$(./bin/main run ../data/feature_train2 ../data/feature_test2 3)
-optimized_result3=$(./bin/main run ../data/feature_train3 ../data/feature_test3 3)
-cd ..
-
 # test openmp
 cd openmp
 make clean && make 
@@ -39,25 +23,9 @@ cd ..
 
 # remove data
 cd data
-rm feature_*
+rm feature_test1 feature_test2 feature_test3 feature_train1 feature_train2 feature_train3
 cd ..
 
-echo ""
-echo "===================="
-echo ""
-# output results
-echo "Baseline result 1 ~ 3:"
-echo $baseline_result1
-echo $baseline_result2
-echo $baseline_result3
-echo ""
-echo "===================="
-echo ""
-echo "Optimized result 1 ~ 3:"
-echo $optimized_result1
-echo $optimized_result2
-echo $optimized_result3
-echo ""
 echo "===================="
 echo ""
 echo "OpenMP result 1 ~ 3:"
